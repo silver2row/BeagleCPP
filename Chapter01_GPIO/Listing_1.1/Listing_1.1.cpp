78f048a758005b272062c86af223d18a100f0dcd
Wilmer Gaona <wgaonar@gmail.com>
2021-02-01 08:29:34 -0600

    Global GPIO source enabled

--- Chapter01_GPIO/Listing_1.1/Listing_1.1.cpp
+++ Chapter01_GPIO/Listing_1.1/Listing_1.1.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-#include "GPIO.h"
+#include "../../Sources/GPIO.h"
 
 using namespace std;
 
 int main()
 {
-    string message = "Main program starting here...";
-    cout << rainbowText(message,"Blue", "Bold") << endl;
-    
-    GPIO ledPin(P8_12, OUTPUT);
+  string message = "Main program starting here...";
+  cout << RainbowText(message,"Blue", "Bold") << endl;
+  
+  GPIO ledPin(P8_12, OUTPUT);
 
-    for (int i = 0; i < 10; i++)   
-    {
-        cout << "Blinking " << i+1 << " times out of " << 10 <<  endl;
-        ledPin.digitalWrite(HIGH);
-        ledPin.delayms(1000);
-        ledPin.digitalWrite(LOW);
-        ledPin.delayms(1000);
-    }
+  for (int i = 0; i < 10; i++)   
+  {
+    cout << "Blinking " << i+1 << " times out of " << 10 <<  endl;
+    ledPin.DigitalWrite(HIGH);
+    ledPin.Delayms(1000);
+    ledPin.DigitalWrite(LOW);
+    ledPin.Delayms(1000);
+  }
 
-    message = "Main program finishes here...";
-    cout << rainbowText(message,"Blue", "Bold") << endl;
-    return 0;
+  message = "Main program finishes here...";
+  cout << RainbowText(message,"Blue", "Bold") << endl;
+  return 0;
 }
--- Chapter01_GPIO/Listing_1.2/Listing_1.2.cpp
+++ Chapter01_GPIO/Listing_1.2/Listing_1.2.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-#include "GPIO.h"
+#include "../../Sources/GPIO.h"
 
 using namespace std;
 
 int main()
 {
 
-    string message = "Main program starting here...";
-    cout << rainbowText(message,"Blue", "White", "Bold") << endl;
-    
-    GPIO buttonPin(P8_08, INPUT);
-    GPIO ledPin(P8_12, OUTPUT);
+  string message = "Main program starting here...";
+  cout << RainbowText(message,"Blue", "White", "Bold") << endl;
+  
+  GPIO buttonPin(P8_08, INPUT);
+  GPIO ledPin(P8_12, OUTPUT);
 
-    int count = 0;
-    while (count < 10)
-    {
-        while (buttonPin.digitalRead() == LOW);
-        count++;
-        cout << "The button was pressed, turning the led ON " 
-             << rainbowText(to_string(count), "Red") << " times" << endl;
-        ledPin.digitalWrite(HIGH);
-        
-        while (buttonPin.digitalRead() == HIGH)
-            ledPin.delayms(10);
-        cout << "The button was released, turning the led OFF" << endl << endl;
-        ledPin.digitalWrite(LOW);
-        ledPin.delayms(10);
-    }
+  int count = 0;
+  while (count < 10)
+  {
+    while (buttonPin.DigitalRead() == LOW);
+    count++;
+    cout << "The button was pressed, turning the led ON " 
+          << RainbowText(to_string(count), "Red") << " times" << endl;
+    ledPin.DigitalWrite(HIGH);
     
-    message = "Main program finishes here...";
-    cout << rainbowText(message,"Blue", "Bold") << endl;
-    return 0;
+    while (buttonPin.DigitalRead() == HIGH)
+        ledPin.Delayms(10);
+    cout << "The button was released, turning the led OFF" << endl << endl;
+    ledPin.DigitalWrite(LOW);
+    ledPin.Delayms(10);
+  }
+  
+  message = "Main program finishes here...";
+  cout << RainbowText(message,"Blue", "Bold") << endl;
+  return 0;
 }
