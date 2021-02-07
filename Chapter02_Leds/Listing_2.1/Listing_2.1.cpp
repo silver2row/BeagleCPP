73613f2919f83d016d86201d72d56eb6a05fc3d9
Wilmer Gaona <wgaonar@gmail.com>
2021-02-07 17:45:35 -0600

    Test

--- Chapter02_Leds/Listing_2.1/Listing_2.1.cpp
+++ Chapter02_Leds/Listing_2.1/Listing_2.1.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
-#include "../../Sources/GPIO.h"
+// #include "../../Sources/GPIO.h"
 #include "../../Sources/LED.h"
 
 using namespace std;
 
 int main()
 {
+  string message = "Main program starting here...";
+  cout << RainbowText(message,"Blue", "White", "Bold") << endl;
+  
+  LED ledPinRed(P8_12);
+  LED ledPinYellow(P8_14);
 
-    string message = "Main program starting here...";
-    cout << RainbowText(message,"Blue", "White", "Bold") << endl;
-    
-    LED ledPinRed(P8_12);
-    LED ledPinYellow(P8_14);
+  message = "Start a blinking on a red led";
+  cout << RainbowText(message, "Red") << endl;
+  ledPinRed.Blink(1000);
 
-    message = "Start a blinking on a red led";
-    cout << RainbowText(message, "Red") << endl;
-    ledPinRed.Blink(1000);
+  message = "Start a blinking on a yellow led";
+  cout << RainbowText(message, "Yellow") << endl;
+  ledPinYellow.Blink(100);
 
-    message = "Start a blinking on a yellow led";
-    cout << RainbowText(message, "Yellow") << endl;
-    ledPinYellow.Blink(100);
-
-    char userInput = '\0';
-    while (userInput != 'y')
-    {
-        message = "Do you want to stop blinking? Enter 'y' for yes: ";
-        cout << RainbowText(message, "Violet");
-        cin >> userInput;
-        if (userInput == 'y') {
-            ledPinRed.StopBlink();
-            ledPinYellow.StopBlink();
-        }
+  char userInput = '\0';
+  while (userInput != 'y')
+  {
+    message = "Do you want to stop blinking? Enter 'y' for yes: ";
+    cout << RainbowText(message, "Violet");
+    cin >> userInput;
+    if (userInput == 'y') {
+      ledPinRed.StopBlink();
+      ledPinYellow.StopBlink();
     }
-     
+  }
+    
 
-    message = "Main program finishes here...";
-    cout << RainbowText(message,"Blue", "White","Bold") << endl;
-    return 0;
+  message = "Main program finishes here...";
+  cout << RainbowText(message,"Blue", "White","Bold") << endl;
+  return 0;
 }
