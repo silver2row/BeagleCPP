56a628a9be59389a64d8d5c53773fbadab17f62a
Wilmer Gaona <wgaonar@gmail.com>
2021-02-10 06:25:31 -0600

    added

---
+++ Sources/PIN_ID.h
@@ -0,0 +1,20 @@
+
+#ifndef PIN_ID_H
+#define PIN_ID_H
+
+/* The gpio number of the pin*/
+enum ID {
+  P8_08 = 67,
+  P8_10 = 68,
+  P8_11 = 45,
+  P8_12 = 44,
+  P8_14 = 26,
+  P8_16 = 46,
+  P8_17 = 27,
+  P8_18 = 65,
+  P8_19 = 22,
+  P8_20 = 63,
+  P8_26 = 61,
+};
+
+#endif // PIN_ID_H
