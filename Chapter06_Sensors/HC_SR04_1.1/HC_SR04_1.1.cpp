5bf4ac4ab6252b4825393ac072963a72baa332dc
Wilmer Gaona <wgaonar@gmail.com>
2021-04-08 21:07:38 -0500

    Updates

--- Chapter06_Sensors/HC_SR04_1.1/HC_SR04_1.1.cpp
+++ Chapter06_Sensors/HC_SR04_1.1/HC_SR04_1.1.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include "../../Sources/GPIO.h"
+#include "../../Sources/BUTTON.h"
 
 using namespace std;
 
 /* auto startTime = std::chrono::steady_clock::now(); */
 
 // Declaring the pins and the HC_SR04 object
-GPIO echoPin(P9_17, INPUT);
+BUTTON echoPin(P9_17);
 GPIO triggerPin(P9_15, OUTPUT);
 
 auto startTime = std::chrono::steady_clock::now();
-
+int counter = 0;
 int userFunction()
 {
   auto endTime = std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsedTime = (endTime-startTime);
-  std::cout << "duration: " << elapsedTime.count(); 
-  std::cout << " / distance: " << elapsedTime.count()*34300 / 2.0 << std::endl;
+  cout << "duration: " << std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+  std::cout << " / duration: " << elapsedTime.count();
+  startTime = std::chrono::steady_clock::now();
+  counter++;
+  cout << " / counter: " << counter << endl;
   return 0;
 }
 
@@ -24,19 +28,19 @@ int main()
 {
   string message = "Main program starting here...";
   cout << RainbowText(message,"Blue", "White", "Bold") << endl;
-  
 
   echoPin.AddEventDetection(RISING, userFunction);
 
+  startTime = std::chrono::steady_clock::now();
   while (true)
   {
     triggerPin.DigitalWrite(HIGH);
-    Delayms(5);
+    Delayus(10);
     triggerPin.DigitalWrite(LOW);
     startTime = std::chrono::steady_clock::now();
-    Delayms(1000);
+  
+    Delayms(100);
   }
-   
   
   message = "Main program finishes here...";
   cout << RainbowText(message,"Blue", "White","Bold") << endl;
