7c9631030a3671a10b1493822162afe0541f488d
Wilmer Gaona <wgaonar@gmail.com>
2021-06-18 14:19:24 -0500

    Remove delay from WriteFile and ReadFile

--- Chapter06_Sensors/HC_SR04_1.1/HC_SR04_1.1.cpp
+++ Chapter06_Sensors/HC_SR04_1.1/HC_SR04_1.1.cpp
@@ -1,45 +1,54 @@
 #include <iostream>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::milliseconds
+#include <unistd.h>       // usleep()
+
 #include "../../Sources/GPIO.h"
 #include "../../Sources/BUTTON.h"
 
 using namespace std;
 
-/* auto startTime = std::chrono::steady_clock::now(); */
-
 // Declaring the pins and the HC_SR04 object
-BUTTON echoPin(P9_17);
 GPIO triggerPin(P9_15, OUTPUT);
+GPIO echoPin(P9_17,INPUT);
 
-auto startTime = std::chrono::steady_clock::now();
-int counter = 0;
-int userFunction()
+double DistanceMeasurement()
 {
-  auto endTime = std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsedTime = (endTime-startTime);
-  cout << "duration: " << std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
-  std::cout << " / duration: " << elapsedTime.count();
-  startTime = std::chrono::steady_clock::now();
-  counter++;
-  cout << " / counter: " << counter << endl;
-  return 0;
+  triggerPin.DigitalWrite(HIGH);
+  this_thread::sleep_for(chrono::milliseconds(500));
+  triggerPin.DigitalWrite(LOW);
+
+  const chrono::time_point<std::chrono::steady_clock> initialTime = chrono::steady_clock::now();
+  auto startTime = chrono::steady_clock::now();
+  auto endTime = chrono::steady_clock::now();
+
+  while (echoPin.DigitalRead() == LOW){
+    startTime = std::chrono::steady_clock::now();
+    //cout << "in echoPin LOW: " << chrono::duration_cast<std::chrono::microseconds>(startTime - initialTime).count() << "µs\n";
+  }
+    
+  
+  while (echoPin.DigitalRead() == HIGH){
+    endTime = chrono::steady_clock::now();
+    //cout << "in echoPin HIGH: " << chrono::duration_cast<std::chrono::microseconds>(endTime - initialTime).count() << "µs\n";
+  }
+  
+  chrono::duration<double> pulseDuration = (endTime-startTime);
+  double distance = pulseDuration.count() * 34300 / 2;
+  return distance;
 }
 
-int main()
-{
+int main() {
   string message = "Main program starting here...";
   cout << RainbowText(message,"Blue", "White", "Bold") << endl;
 
-  echoPin.AddEventDetection(RISING, userFunction);
-
-  startTime = std::chrono::steady_clock::now();
-  while (true)
-  {
+  while (true) {
     triggerPin.DigitalWrite(HIGH);
-    Delayus(10);
+    Delayus(250);
     triggerPin.DigitalWrite(LOW);
-    startTime = std::chrono::steady_clock::now();
-  
-    Delayms(100);
+    Delayus(250);
+
+    // double distance = DistanceMeasurement();
   }
   
   message = "Main program finishes here...";
