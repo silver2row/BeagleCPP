25e6598c0ac4aa4c231bd719a8595f0255ceb9a4
Wilmer Gaona <wgaonar@gmail.com>
2021-06-23 23:14:16 -0500

    Examples added

--- Chapter06_Sensors/HC_SR04/HC_SR04_1.3/HC_SR04_1.3.cpp
+++ Chapter06_Sensors/HC_SR04/HC_SR04_1.3/HC_SR04_1.3.cpp
@@ -1,10 +1,11 @@
 /******************************************************************************
-HC_SR04_1.2.cpp
+HC_SR04_1.3.cpp
 @wgaonar
 22/06/2021
 https://github.com/wgaonar/BeagleCPP
 
-Uses a Median filter for the sensor readings 
+Uses a Median filter for 10 sensor readings each second. 
+The sensor object is constructed with offset and temperature correction factors
 
 Class: HC_SR04
 ******************************************************************************/
@@ -20,27 +21,19 @@ using namespace std;
 GPIO triggerPin(P9_15);
 GPIO echoPin(P9_17);
 
-// Declaring the pins and the HC_SR04 object
-HC_SR04 distanceSensor (triggerPin, echoPin);
+// Declaring the pins and the HC_SR04 object with offset and temperature correction factors
+HC_SR04 distanceSensor (triggerPin, echoPin, 1, 25);
 
 
 int main() {
   string message = "Main program starting here...";
   cout << RainbowText(message,"Blue", "White", "Bold") << endl;
 
-  double distance = distanceSensor.MeasureDistanceCmWithMedian(7);
-  int count = 0;
-  while (true) {
-    cout << "Distance reading: " << count << " = " << distance << "cm\n";
-    count++;
-    Delayms(250);
-    if (distance <= 5) {
-      cout << "Too close! Exiting...\n";
-      break;
-    }
-    else {
-      distance = distanceSensor.MeasureDistanceCmWithMedian(7);
-    }
+  double distance = distanceSensor.MeasureDistanceCmWithMedian(5);
+  for (size_t i = 0; i < 10; i++) {
+    distance = distanceSensor.MeasureDistanceCmWithMedian(5);;
+    cout << "Distance reading: " << i << " = " << distance << "cm\n";
+    Delayms(1000);
   }
 
   message = "Main program finishes here...";
