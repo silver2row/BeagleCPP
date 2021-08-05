9cb8166275341648c016e5adddad5fcad99f176b
Wilmer Gaona <wgaonar@gmail.com>
2021-08-05 13:41:05 -0500

    LM35 class added

---
+++ Sources/LM35.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include "LM35.h"
+
+// Overload Constructor with dataPin
+LM35::LM35 (ADC newADCPin) : 
+            ADCPin(newADCPin)
+{ 
+  InitSensor();
+
+  std::string message;
+  message = "LM35 object with Vout on pin: " +
+            this->ADCPin.GetPinHeaderId() +
+            " was created!\n\n";
+  std::cout << RainbowText(message, "Violet");
+}
+
+/*
+  Private method to prepare the sensor for reading data
+*/
+void LM35::InitSensor() 
+{
+  // Security before start the reading
+  Delayms(100);
+}
+
+
+/*
+  Private method to read and store the data from the sensor
+*/
+void LM35::ReadDataFromSensor() 
+{
+  double voltageFromSensor = ADCPin.ReadVoltage();
+
+  temperatureCelsius = voltageFromSensor * 100;
+  temperatureFahrenheit = temperatureCelsius * 1.8 + 32;
+}
+
+
+/*
+  Public method to get the temperature in °C
+  @return double: the measured temperature (2°C - 150°C)
+*/
+double LM35::ReadTemperatureCelsius() 
+{
+  // Get the reading
+  this->ReadDataFromSensor();
+
+  std::string message;
+  message = "Temperature: " + std::to_string(temperatureCelsius) + " °C\n";
+  std::cout << RainbowText(message, "Neon Green");
+
+  return temperatureCelsius; 
+}
+
+/*
+  Public method to get the temperature in °F
+  @return double: the measured temperature (35.6°F - 302°F)
+*/
+double LM35::ReadTemperatureFahrenheit() 
+{
+  // Get the reading
+  this->ReadDataFromSensor();
+
+  std::string message;
+  message = "Temperature: " + std::to_string(temperatureFahrenheit) + " °F\n";
+  std::cout << RainbowText(message, "Neon Green");
+
+  return temperatureFahrenheit; 
+}
+
+// Destructor
+LM35::~LM35() {}
---
+++ Sources/LM35.h
@@ -0,0 +1,34 @@
+#ifndef LM35_H
+#define LM35_H
+
+#include "ADC.h"
+
+class LM35 : public ADC 
+{
+private:
+  ADC ADCPin;
+  double temperatureCelsius;
+  double temperatureFahrenheit;
+
+  // Initialize the sensor with the data provided by the constructor
+  virtual void InitSensor();
+
+  // Read data from the sensor
+  virtual void ReadDataFromSensor();
+
+public:
+
+  // Overload constructor with trigger and echo pins
+  LM35(ADC);
+
+  // Public method to read the Temperature in °C
+  double ReadTemperatureCelsius();
+  
+  // Public method to read the Temperature in °F
+  double ReadTemperatureFahrenheit();
+
+  // Destructor
+  ~LM35();
+};
+
+#endif // LM35_H
