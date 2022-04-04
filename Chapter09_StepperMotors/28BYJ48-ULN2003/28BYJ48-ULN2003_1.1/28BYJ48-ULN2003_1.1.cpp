03212bc3ddf591b6d477089b321aea6a9ce630e2
Wilmer Gaona <wgaonar@gmail.com>
2022-04-03 23:49:32 -0500

    Example added

---
+++ Chapter09_StepperMotors/28BYJ48-ULN2003/28BYJ48-ULN2003_1.1/28BYJ48-ULN2003_1.1.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+28BYJ48-ULN2003_1.1.cpp
+@wgaonar
+03/04/2022
+https://github.com/wgaonar/BeagleCPP
+
+- Move the stepperMotor by steps
+
+Class: STEPPERMOTOR
+******************************************************************************/
+
+#include <iostream>
+#include "../../../Sources/STEPPERMOTOR.h"
+
+using namespace std;
+
+// Declaring the pins for motor
+GPIO IN1 (P8_12);
+GPIO IN2 (P8_14);
+GPIO IN3 (P8_16);
+GPIO IN4 (P8_18);
+
+// Declare the motor object
+StepperMotor myStepper (IN1, IN2, IN3, IN4);
+
+int main()
+{
+  std::string message = "Main program starting here...";
+  cout << RainbowText(message,"Blue", "White", "Bold") << endl;
+
+  /* 
+    Turn the stepper motor 1/4-turn in CW direction in fullstep 
+    mode with 2 coils at the same time at 500 steps/second
+  */
+  myStepper.TurnBySteps(512);
+
+  /* 
+    Turn the stepper motor 1/4-turn in CCW direction in fullstep 
+    mode with 2 coils at the same time at 500 steps/second
+  */
+  myStepper.TurnBySteps(-512);
+
+  message = "Main program finishes here...";
+  cout << RainbowText(message,"Blue", "White","Bold") << endl;
+
+  return 0;
+}
